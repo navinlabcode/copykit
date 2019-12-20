edf9eb339d2c56bc1dce26ad258617b543640722
darlanminussi <darlanminussi@gmail.com>
2019-12-20 16:07:21 -0600

    fix classes

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -10,11 +10,11 @@
 #'
 
 #' @export
-#' @importClassesFrom SummarizedExperiment RangedSummarizedExperiment SingleCellExperiment
+#' @importClassesFrom SingleCellExperiment SingleCellExperiment
 .scCNA <- setClass("scCNA", contains = "SingleCellExperiment")
 
 #' @export
-#' @importClassesFrom SummarizedExperiment RangedSummarizedExperiment SingleCellExperiment
+#' @importClassesFrom SingleCellExperiment SingleCellExperiment
 scCNA <- function(segment_ratios,
                   ratios,
                   bin_counts,
