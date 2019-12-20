2e3f08afc21e73587fa7d5aaedb636a11c3f5d4b
darlanminussi <darlanminussi@gmail.com>
2019-12-20 12:13:09 -0600

    added scCNA show method

--- R/internals.R
+++ R/internals.R
@@ -4,22 +4,31 @@
 #' @description
 #' Methods to get or set internal fields from the scCNA class
 
-#' export
+#' @export
 setMethod("segment_ratios", "scCNA", function(x, withDimnames = TRUE) {
   # accessor for the segment_ratios data within the assay slot
   SummarizedExperiment::assay(x, "segment_ratios")
 })
 
 
-#' export
+#' @export
 setMethod("ratios", "scCNA", function(x, withDimnames = TRUE) {
   # accessor for the ratios data slot
   SummarizedExperiment::assay(x, "ratios")
 })
 
-#' export
+#' @export
 setMethod("bin_counts", "scCNA", function(x, withDimnames = TRUE) {
   # accessor for the bin_counts data slot
   SummarizedExperiment::assay(x, "bin_counts")
 })
 
+#' @export
+#' @importMethodsFrom SingleCellExperiment show
+setMethod("show", "scCNA", function(object) {
+  callNextMethod()
+  cat(
+    "rowRanges has: ", length(SummarizedExperiment::rowRanges(cna_obj)), " ranges",
+    sep = ""
+  )
+})
