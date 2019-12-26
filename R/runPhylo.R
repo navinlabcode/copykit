fa96908a73160f9cf6c7420c3e9b42e07911766e
darlanminussi <darlanminussi@gmail.com>
2019-12-26 13:35:03 -0600

    runPhylo uses distMat slot

--- R/runPhylo.R
+++ R/runPhylo.R
@@ -6,10 +6,10 @@
 #'
 #' @param scCNA scCNA object.
 #' @param method Phylogenetic method to be run, currently only accepts "nj" (neighbor-joining).
-#' @param metric distance metric passed to construct the phylogeny.
+#' @param metric distance metric passed to construct the phylogeny (Defaults to "euclidean").
 #' @param n_threads Number of threads used to calculate the distance matrix. Passed to `amap::Dist`
 #'
-#' @return A reduced dimension representation with UMAP in the slot \code{reducedDim} from scCNA object. Access reduced dimensions slot with: \code{SingleCellExperiment::reducedDim(scCNA, 'umap', withDimnames = FALSE)}
+#' @return A reduced dimension representation with UMAP in the slot \code{reducedDim} from scCNA object. Access reduced dimensions slot with: \code{SingleCellExperiment::reducedDim(scCNA, 'umap')}
 #' @export
 #'
 #' @examples
@@ -19,6 +19,17 @@ runPhylo <- function(scCNA,
                      metric = "euclidean",
                      n_threads = 1) {
 
+  # checking distance matrix
+  if (length(copykit::distMat(scCNA)) == 0) {
+    message("No distance matrix detected in the scCNA object.")
+    scCNA <-  runDistMat(scCNA, metric = metric)
+  }
+
+  if (nrow(as.matrix(copykit::distMat(scCNA))) != ncol(scCNA)) {
+    stop("Number of samples in the distance matrix different from number of samples in the scCNA object. Perhaps you filtered your dataset? use copykit::runDistMat() to update it.")
+  }
+
+  # getting data
   seg_data <- t(segment_ratios(scCNA)) %>%
     as.data.frame()
 
@@ -30,7 +41,8 @@ runPhylo <- function(scCNA,
   }
 
   # ordering cells
-  tree <- ape::nj(amap::Dist(seg_data, nbproc = n_threads))
+  message("Creating neighbor-joining tree.")
+  tree <- ape::nj(distMat(scCNA))
 
   tree <- ape::ladderize(tree)
 
