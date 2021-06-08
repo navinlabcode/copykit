c48ad539053b06fb9c97cf64ec82de45b946d8b6
darlanminussi <darlanminussi@gmail.com>
2021-06-08 17:38:13 -0500

    better calcInteger method scalable to different methods

--- R/calcInteger.R
+++ R/calcInteger.R
@@ -1,25 +1,33 @@
 #' Calculates the integer copy number profile for each single-cell
 #'
 #' @param scCNA The scCNA object.
-#' @param method Method used to calculate integer profiles. Options: 'fixed'.
-#' @param ploidy_value If method of choice is 'fixed' a ploidy value should be provided
+#' @param method Method used to scale the ratio values to integer.
+#' @param ploidy_value If method of choice is 'fixed' a ploidy value should be provided.
+#' @param slot Name of the resulting slot where integer matrix will be saved.
 #'
 #' @details
+#' \itemize{
 #' \item{fixed:} When method argument is set to 'fixed' copykit extracts the segment means
 #' from the scCNA object and multiplies those means by the value provided in the
 #' argument ploidy_value.
+#' }
 #'
 #' @return The scCNA object with an assay slot named 'integer' that contains
 #' a data frame with cells as columns and integerized segments as rows.
 #' @export
 #'
+#' @importFrom S4Vectors metadata
+#' @importFrom SummarizedExperiment assay
+#'
 #' @examples
 calcInteger <- function(scCNA,
-                        method,
-                        ploidy_value = NULL) {
+                        method = 'fixed',
+                        ploidy_value = NULL,
+                        slot = 'integer') {
   seg_ratios_df <- copykit::segment_ratios(scCNA)
 
-  if (method == "fixed") {
+  if (method == 'fixed') {
+
     if (is.null(ploidy_value) && !is.numeric(ploidy_value)) {
       stop("Method fixed requires a numeric value for ploidy_value argument.")
     }
@@ -27,13 +35,32 @@ calcInteger <- function(scCNA,
     message(paste("Scaling ratio values by ploidy value",
                   ploidy_value))
 
-    int_values <- round(seg_ratios_df * ploidy_value)
+    # ploidy values are added to colData information
+    colData(scCNA)$ploidy_value <- ploidy_value
 
-    assay(scCNA, 'integer') <- int_values
+    # saving ploidy scaling method
+    S4Vectors::metadata(scCNA)$ploidy_method <- 'fixed'
+
+  }
+
+    # check to guarantee multiplication
+    if (!identical(names(seg_ratios_df), colData(scCNA)$sample)) {
+      stop("Order of cells in segment_ratios slot and colData(scCNA) is not identical.")
+    }
+
+    # obtain the matrix of integer values by multiplying the seg ratios
+    # by the diagonal of the ploidy colData vector
+    int_values <- round(as.matrix(seg_ratios_df) %*% diag(colData(scCNA)$ploidy_value)) %>%
+      as.data.frame()
+
+    # recovering names
+    names(int_values) <- names(seg_ratios_df)
+
+    assay(scCNA, slot) <- int_values
 
     return(scCNA)
 
-  }
+
 
 }
 
