dc4eb95f2d505285d0f7935a7b4a247c484a245a
darlanminussi <darlanminussi@gmail.com>
2021-09-14 14:15:19 -0500

    adding plotAlluvial

---
+++ R/plotAlluvial.R
@@ -0,0 +1,104 @@
+#' plotAlluvial
+#'
+#' Produces an alluvial plot from character elements of the metadata
+#'
+#' @param scCNA
+#' @param label A string with two or more  elements from \code{\link[SummarizedExperiment]{colData}}.
+#' @param label_colors An optional named vector with the colors of each element from label.
+#'
+#' @return A ggplot object containing an alluvial plot from ggalluvial
+#'
+#' @importFrom ggalluvial geom_flow geom_stratum to_lodes_from
+#' @importFrom SummarizedExperiment colData
+#' @importFrom dplyr all_of across group_by count
+#' @import ggplot2
+#'
+#' @export
+#'
+#' @examples
+plotAlluvial <- function(scCNA,
+                         label,
+                         label_colors = NULL) {
+  meta <- as.data.frame(colData(scCNA))
+
+  # check
+  if (all(is.numeric(meta[label]))) {
+    stop("label argument must not contain numeric columns.")
+  }
+
+  # calculating frequencies across labels
+  alluvial_dat  <- meta %>%
+    dplyr::group_by(dplyr::across(dplyr::all_of(label))) %>%
+    dplyr::count() %>%
+    ggalluvial::to_lodes_form(
+      key = 'class',
+      value = 'group',
+      id = 'cohort',
+      axes = 1:length(label)
+    )
+
+  # managing colors
+  if (is.null(label_colors)) {
+    # defaults
+    label_colors <- c(
+      superclones_pal(),
+      subclones_pal(),
+      c("removed" = "#DA614D",
+        "kept" = "#5F917A"),
+      c("TRUE" = "#396DB3",
+        "FALSE" = "#11181D")
+    )
+
+    # non defaults
+    non_default <- label[label %!in% c("superclones",
+                                       "subclones",
+                                       "is_normal",
+                                       "filtered")]
+
+    non_default_colors <- vector(mode = 'list')
+
+    for (i in seq_along(non_default)) {
+      # luminescence increase
+      l <- 0
+      groups_label <- unique(meta[[non_default[i]]])
+
+      non_default_colors[[i]] <-
+        structure(scales::hue_pal()(length(groups_label)),
+                  names = groups_label)
+      names(non_default_colors)[i] <- non_default[i]
+
+      l <- l + 15
+
+    }
+
+    label_colors <-
+      c(label_colors, unlist(unname(non_default_colors)))
+
+  }
+
+  # plot
+  p <- ggplot(data = alluvial_dat,
+              aes(
+                x = class,
+                stratum = group,
+                alluvium = cohort,
+                y = n
+              )) +
+    ggalluvial::geom_flow(
+      aes(fill = group),
+      stat = "alluvium",
+      color = "black",
+      alpha = .7,
+      width = 1 / 8
+    ) +
+    ggalluvial::geom_stratum(aes(fill = group), color = 'black', width = 1 / 8) +
+    geom_text(stat = "stratum", aes(label = group)) +
+    theme_void() +
+    theme(legend.position = 'none',
+          axis.text.x = element_text(color = 'black')) +
+    scale_fill_manual(values = label_colors,
+                      limits = force)
+
+  print(p)
+
+}
