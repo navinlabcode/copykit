aa0dbb78d2408bebb79caf68e212fc3083312b2b
darlanminussi <darlanminussi@gmail.com>
2021-10-15 17:25:14 -0500

    adding piecharts to plotPhylo

--- R/plotPhylo.R
+++ R/plotPhylo.R
@@ -1,4 +1,4 @@
-#' Plot phylogenetic tree
+#' plotPhylo()
 #'
 #' Plots a phylogenetic tree.
 #'
@@ -11,21 +11,28 @@
 #'  Must match label length
 #' @param consensus A boolean indicating if the consensus phylogenetic tree
 #' should be plotted.
+#' @param group A string that if provided will plot the tip labels as pie charts
+#' with the proportions from the provided element from
+#' \code{\link[SummarizedExperiment]{colData}}
 #'
-#' @return A ggplot object with aphylogenetic tree visualization.
+#' @return A ggplot object with a phylogenetic tree visualization.
 #' @export
 #'
 #' @import ggtree
 #' @importFrom scales hue_pal
+#' @importFrom rlang sym
+#' @importFrom stringr str_remove
+#' @importFrom dplyr select
+#' @importFrom tidyr gather
 #'
 #' @examples
 #'
 
 plotPhylo <- function(scCNA,
-					  label = NULL,
-					  label_colors = NULL,
-					  consensus = FALSE){
-
+                      label = NULL,
+                      label_colors = NULL,
+                      consensus = FALSE,
+                      group = NULL) {
   # consensus and not consensus logic
   if (consensus == FALSE) {
     tryCatch(
@@ -38,11 +45,12 @@ plotPhylo <- function(scCNA,
     tree <- phylo(scCNA)
 
   } else {
-
-  	tryCatch(
+    tryCatch(
       consensusPhylo(scCNA),
       error = function(e) {
-        stop("No consensus phylogeny detected in scCNA object. Please run runConsensusPhylo() first.")
+        stop(
+          "No consensus phylogeny detected in scCNA object. Please run runConsensusPhylo() first."
+        )
       }
     )
 
@@ -51,39 +59,85 @@ plotPhylo <- function(scCNA,
   }
 
 
-    # plotting
+  # plotting
   if (is.null(label)) {
     # plotting without labels
 
     ggtree::ggtree(tree, ladderize = F)
 
   } else {
-  	# plotting with labels
-  	if ( length(label)>1 ) {
-  		stop("Only one label can be visualized in the tree.")
-  	}
+    # plotting with labels
+    if (length(label) > 1) {
+      stop("Only one label can be visualized in the tree.")
+    }
 
     metadata <- SummarizedExperiment::colData(scCNA) %>%
       as.data.frame()
 
     if (!(label %in% colnames(metadata))) {
-    	stop(paste0("Label ", label, " is not a column of the scCNA object."))
-  	}
-
-	metadata_anno_df <- metadata %>% dplyr::select(dplyr::all_of(label))
-
-    if (consensus == FALSE) {
-
-    	metadata_anno_df <- metadata_anno_df[tree$tip.label,, drop=F]
-    	size <- 1
-
+      stop(paste0("Label ", label, " is not a column of colData(scCNA)."))
     }
 
+    metadata_anno_df <-
+      metadata %>% dplyr::select(dplyr::all_of(label))
+
+    if (consensus == FALSE) {
+      metadata_anno_df <- metadata_anno_df[tree$tip.label, , drop = F]
+      size <- 1
+
+    }
 
     if (consensus == TRUE) {
       # Uses the hidden consensus_by attribute from the calcConsensus function
       # to guarantee the same order
       cons_attr <- attr(consensus(scCNA), "consensus_by")
+
+      if (!is.null(group)) {
+        # check if group exists
+        if (!is.null(group) && !(group %in% colnames(metadata))) {
+          stop(paste0("Group ", group, " is not a column of colData(scCNA)."))
+        }
+
+        # data for groups
+        metadata_gr <- metadata %>%
+          droplevels() %>%
+          dplyr::select(!!cons_attr,!!group) %>%
+          tidyr::gather(key = 'consensus_var',
+                        value = 'group_value',-!!cons_attr)
+
+        names(metadata_gr)[1] <- 'cons_attr'
+
+        group_split <- metadata_gr$cons_attr
+        metadata_gr_list <- split(metadata_gr, group_split)
+
+        # colors for groups
+        elements_groups <- unique(metadata_gr$group_value)
+        n_groups <- length(elements_groups)
+        hex <- scales::hue_pal()(n_groups)
+
+        col <- structure(hex,
+                         names = elements_groups)
+
+        group_colors <- col
+
+        pies <- lapply(metadata_gr_list, function(x) {
+          ggplot(x,
+                 aes(
+                   y = cons_attr,
+                   fill = group_value,
+                   x = ""
+                 )) +
+            geom_bar(stat = 'identity') +
+            coord_polar(theta = 'y', start = 0) +
+            theme_void() +
+            theme(legend.position = "none") +
+            scale_fill_manual(values = group_colors)
+
+        })
+
+        names(pies) <- names(metadata_gr_list)
+
+      }
 
       metadata_anno_df <- metadata_anno_df[label] %>%
         dplyr::distinct()
@@ -99,7 +153,6 @@ plotPhylo <- function(scCNA,
     }
 
     if (is.null(label_colors)) {
-
       #default colors superclones and subclones
       label_colors <- c(
         list(
@@ -112,54 +165,69 @@ plotPhylo <- function(scCNA,
         )
       )
 
-        if (any(str_detect(
-          label,
-          c("superclones", "subclones", "filtered", "is_normal")
-        ))) {
-          # if label is one of the four above, uses the default specifed colors above
-          label_colors <- label_colors[[label]]
+      if (any(str_detect(
+        label,
+        c("superclones", "subclones", "filtered", "is_normal")
+      ))) {
+        # if label is one of the four above, uses the default specifed colors above
+        label_colors <- label_colors[[label]]
 
-        } else {
-          # if label is not numeric
+      } else {
+        # if label is not numeric
 
-          elements <- metadata_anno_df %>%
-            dplyr::pull(label) %>%
-            unique() %>%
-            as.character() %>%
-            sort()
+        elements <- metadata_anno_df %>%
+          dplyr::pull(label) %>%
+          unique() %>%
+          as.character() %>%
+          sort()
 
-          n <- length(elements)
-          hues <- seq(15, 375, length = n + 1)
-          hex <- scales::hue_pal()(n)
+        n <- length(elements)
+        hues <- seq(15, 375, length = n + 1)
+        hex <- scales::hue_pal()(n)
 
-          col <- structure(hex,
-                           names = elements)
+        col <- structure(hex,
+                         names = elements)
 
-          label_colors <- col
-
-
-        }
+        label_colors <- col
 
 
+      }
 
     } else if (is.list(label_colors)) {
-    	label_colors <- label_colors[[label]]
+      label_colors <- label_colors[[label]]
     }
 
 
-    list_samples <- split(rownames(metadata_anno_df), metadata_anno_df[,label])
+    list_samples <-
+      split(rownames(metadata_anno_df), metadata_anno_df[, label])
     tree <- ggtree::groupOTU(tree, list_samples)
-    p <- ggtree::ggtree(tree, ladderize=F) +
-    	geom_tippoint(aes(color=group), size=size) +
-    	scale_color_manual(values=label_colors, name=label) +
+    p <- ggtree::ggtree(tree, ladderize = F) +
+      geom_tippoint(aes(color = group), size = size) +
+      scale_color_manual(values = label_colors, name = label) +
       theme(legend.position = 'none')
 
-    if(consensus){
-    	p <- p + geom_tiplab(aes(color=group), size=5, hjust=-0.5)
+    if (consensus) {
+      p <- p + geom_tiplab(aes(color = group), size = 5, hjust = -0.5)
+
+      if (!is.null(group)) {
+        # getting tree order
+        ord <- p[['data']][['label']]
+        #ordering plots by tree order
+        names(pies) <- match(names(pies), ord[!is.na(ord)])
+        # removing strings to be compatible with inset func
+        p <-
+          ggtree::inset(p,
+                        pies,
+                        width = 0.08,
+                        height = 0.08,
+                        hjust = 0)
+
+      }
+
     }
 
-    p
+    print(p)
+
   }
 
 }
-
