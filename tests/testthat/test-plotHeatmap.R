4bd2a14178fb90e947c493527e0208a048377d7b
darlanminussi <darlanminussi@gmail.com>
2021-12-05 18:15:47 -0600

    adding plotHeatmap test

---
+++ tests/testthat/test-plotHeatmap.R
@@ -0,0 +1,29 @@
+# setup
+copykit_obj <- copykit_example_filtered()
+set.seed(1000)
+copykit_obj <- copykit_obj[, sample(70)]
+copykit_obj <- findClusters(copykit_obj)
+copykit_obj <- calcConsensus(copykit_obj)
+copykit_obj <- runConsensusPhylo(copykit_obj)
+colData(copykit_obj)$section <- stringr::str_extract(colData(copykit_obj)$sample,
+                                                     "(L[0-9]+L[0-9]+|L[0-9]+)")
+
+#tests
+
+test_that("Testing plotting heatmap: ", {
+  expect_s4_class(ht <- plotHeatmap(copykit_obj), "Heatmap")
+
+  # checking visual parameters
+  expect_s4_class(ht <-
+                    plotHeatmap(copykit_obj, label = "subclones"),
+                  "Heatmap")
+  expect_s4_class(ht <- plotHeatmap(copykit_obj,
+                                    label = c("section", "subclones")),
+                  "Heatmap")
+  expect_s4_class(ht <- plotHeatmap(copykit_obj, genes = c("MYC",
+                                                           "TP53")),
+                  "Heatmap")
+  expect_s4_class(ht <- plotHeatmap(copykit_obj, group = "section"),
+                  "Heatmap")
+
+})
