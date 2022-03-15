090c556ee81adb931d06cda0dc3cb3a3af4e3931
darlanminussi <darlanminussi@gmail.com>
2022-03-15 13:09:26 -0500

    reducing default quality of heatmap raster

--- R/plotHeatmap.R
+++ R/plotHeatmap.R
@@ -125,7 +125,7 @@ plotHeatmap <- function(scCNA,
     col = NULL,
     row_split = NULL,
     use_raster = TRUE,
-    raster_quality = 5,
+    raster_quality = 2,
     n_threads = 1) {
     # args
     order_cells <- match.arg(order_cells)
