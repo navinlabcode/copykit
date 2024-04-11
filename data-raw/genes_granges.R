9cdeb43e260faf8ed5fddd7ca3486bf4397a1976
akcasasent <31112385+akcasasent@users.noreply.github.com>
2024-04-11 16:18:33 -0500

    Create mouse Development branch
    New mouse Branch in order to work on mm10 and other gRanges and scaffolds

--- data-raw/genes_granges.R
+++ data-raw/genes_granges.R
@@ -1,11 +1,11 @@
 ##########################################
-# Goal: Generate the GRanges object for the hg19 varbins
+# Goal: Generate the GRanges object for the different varbins
 # containing gene symbols
 #
-# # Output: hg19_genes object used in sysdata.rda
+# Output: {genome_name}_genes object used in sysdata.rda
 #
 ##########################################
-# Author: Darlan Conterno Minussi
+# Author: Darlan Conterno Minussi & Anna K. Casasent
 ##########################################
 
 library(AnnotationHub)
@@ -28,3 +28,37 @@ src <- src_organism("TxDb.Hsapiens.UCSC.hg38.knownGene")
 hg38_genes <- genes(src, columns = "symbol")
 
 usethis::use_data(hg38_genes)
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+# mm10 genes
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+
+library(AnnotationHub)
+# BiocManager::install("Mus.musculus")
+library(Mus.musculus)
+library(TxDb.Mmusculus.UCSC.mm10.knownGene)
+# BiocManager::install("Organism.dplyr")
+library(Organism.dplyr)
+
+src <- src_organism("TxDb.Mmusculus.UCSC.mm10.knownGene")
+mm10_genes <- genes(src, columns = "symbol")
+saveRDS(mm10_genes, "data/mm10_genes.rda")
+usethis::use_data(mm10_genes)
+
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+## mm39 genes
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+## This requires R 4.3 
+## Plans to add when updating R
+#library(AnnotationHub)
+## BiocManager::install("Mus.musculus")
+#library(Mus.musculus)
+## BiocManager::install("TxDb.Mmusculus.UCSC.mm39.knownGene")
+#library(TxDb.Mmusculus.UCSC.mm39.knownGene)
+#library(Organism.dplyr)
+#
+#src <- src_organism("TxDb.Mmusculus.UCSC.mm39.knownGene")
+#mm39_genes <- genes(src, columns = "symbol")
+#saveRDS(mm39_genes, "data/mm39_genes.rda")
+#usethis::use_data(mm39_genes)
+
