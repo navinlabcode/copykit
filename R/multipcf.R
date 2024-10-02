e8396c5687b02cb1bf83d239e19fcebd3c03837a
darlanminussi <darlanminussi@gmail.com>
2024-10-02 07:30:52 -0700

    adding missing function

--- R/multipcf.R
+++ R/multipcf.R
@@ -1114,3 +1114,52 @@ pullOutContent <- function(res,what="segments"){
   return(res)
   
 }
+
+####################################################################
+## Author: Gro Nilsen, Knut Liest?l and Ole Christian Lingj?rde.
+## Maintainer: Gro Nilsen <gronilse@ifi.uio.no>
+## License: Artistic 2.0
+## Part of the copynumber package
+## Reference: Nilsen and Liest?l et al. (2012), BMC Genomics
+####################################################################
+
+
+# Function to calculate running median for a given a window size
+
+##Input:
+### x: vector of numeric values
+### k: window size to be used for the sliding window (actually half-window size)
+
+## Output:
+### runMedian : the running median corresponding to each observation
+
+##Required by:
+### getMad
+### medianFilter
+
+
+##Requires:
+### none
+
+#' @export
+medianFilter <- function(x,k){
+  n <- length(x)
+  filtWidth <- 2*k + 1
+  
+  #Make sure filtWidth does not exceed n
+  if(filtWidth > n){
+    if(n==0){
+      filtWidth <- 1
+    }else if(n%%2 == 0){
+      #runmed requires filtWidth to be odd, ensure this:
+      filtWidth <- n - 1
+    }else{
+      filtWidth <- n
+    }
+  }
+  
+  runMedian <- runmed(x,k=filtWidth,endrule="median")
+  
+  return(runMedian)
+  
+}
